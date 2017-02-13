59444580b1a35ca1872bc9053cde18004bfaca9a
sergeyarhipov <s.m.arhipov@mail.ru>
2017-02-13 17:40:37 +0500

    Изменен JS, внесены изменения по стилям

--- js/script.js
+++ js/script.js
@@ -13,3 +13,40 @@ closeBasket.addEventListener("click", function(event) {
   event.preventDefault();
   basket.classList.remove("popup-basket-show");
   });
+
+
+var btnSend = document.querySelector(".btn-map");
+var send = document.querySelector(".popup-send");
+var closeSend = document.querySelector(".popup-close");
+var form = send.querySelector("form")
+var userName = document.querySelector("[name=user-name]");
+var userEmail = document.querySelector("[name=user-email]");
+var nameStorage = localStorage.getItem("userName")
+var emailStorage = localStorage.getItem("userEmail")
+
+btnSend.addEventListener("click", function(event) {
+  event.preventDefault();
+  send.classList.add("popup-send-show");
+
+    if (nameStorage) {
+      userName.value = nameStorage;
+      userEmail.focus();
+    } else {
+      userName.focus();
+    }
+  userName.focus();
+  });
+
+form.addEventListener("submit", function(event) {
+  if (!userName.value || !userEmail) {
+    event.preventDefault();
+  } else {
+    localStorage.setItem("user-name", userName.value);
+    localStorage.setItem("user-email", userEmail.value);
+  }
+});
+
+closeSend.addEventListener("click", function(event) {
+  event.preventDefault();
+  send.classList.remove("popup-send-show");
+});
